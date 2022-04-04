2138dcafc52354bf0bfbbcc583d731b16d704396
BuildTools <unconfigured@null.spigotmc.org>
2022-04-04 00:24:04 -0600

    Updated status

--- src/events/ready.js
+++ src/events/ready.js
@@ -11,11 +11,11 @@ module.exports = {
         client.user.setPresence({
             activities: [
                 {
-                    name: 'ALPHA V0.0 🐈',
-                    type: 'WATCHING',
+                    name: 'HUNTING UPDATE v1.0 🗡️',
+                    type: 'PLAYING',
                 },
             ],
-            status: 'dnd',
+            status: 'online',
         });
 
         let boot = client.user.tag + ' is now online!';

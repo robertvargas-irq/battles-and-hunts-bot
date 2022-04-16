25dda7239a47d3e2c076674ac8ee2ad1967e0904
BuildTools <unconfigured@null.spigotmc.org>
2022-04-15 19:58:40 -0600

    Fully implemented command restrictions

--- src/events/interactions/command.js
+++ src/events/interactions/command.js
@@ -1,7 +1,7 @@
 const { MessageEmbed, Permissions } = require('discord.js');
 const fs = require('fs');
 
-const restrictedChannels = new Map(require('./restrictions.json').ChannelToSpecific.map(([k,v]) => [k, new Set(v)]));
+const restrictions = require('./restrictions.json');
 
 module.exports = async ( interaction ) => {
 
@@ -20,24 +20,10 @@ module.exports = async ( interaction ) => {
     // execute command
     try {
         // error if the channel id is restricted and wrong command is used
-        let check = restrictedChannels.get(interaction.channel.id);
-        if (!interaction.member.permissions.has(Permissions.FLAGS.MANAGE_CHANNELS)
-        && restrictedChannels.has(interaction.channel.id)) {
-            if (!check.has(interaction.commandName)) {
-                return interaction.reply({
-                    ephemeral: true,
-                    embeds: [new MessageEmbed()
-                        .setColor('BLUE')
-                        .setTitle('❗ __Woah There!__')
-                        .setDescription('**This channel (<#'
-                        + interaction.channel.id
-                        + ">) is only for the following commands:\n"
-                        + Array.from(check.values()).map(v => "`/" + v + "`").join('\n')
-                        + "\n\n**Please only use these commands in this channel!**\nThank you! ❣️"
-                    )]
-                })
-            }
-        }
+        let wrongId = wrongChannel(interaction);
+        console.log({wrongId});
+        if (wrongId) return wrongChannelMessage(interaction, wrongId);
+
         await interaction.client.commands.get( interaction.commandName ).execute( interaction ).catch();
     }
     catch ( error ) {
@@ -60,4 +46,53 @@ module.exports = async ( interaction ) => {
         }
 
     }
+}
+
+
+
+
+function wrongChannel(interaction) {
+
+    // // administrator override
+    // if (interaction.member.permissions.has(Permissions.FLAGS.MANAGE_CHANNELS))
+    // return false;
+
+    // if guild has no data return
+    const R_GUILD = restrictions[interaction.guild.id];
+    console.log({R_GUILD});
+    if (!R_GUILD) return false;
+
+    // get channel restrictions
+    const R_CHANNEL = R_GUILD.CHANNELS[interaction.channel.id];
+    const R_COMMAND = R_GUILD.COMMANDS[interaction.commandName];
+
+    console.log({R_CHANNEL, R_COMMAND});
+    // if no restrictions, return
+    if (!R_CHANNEL && !R_COMMAND) return false;
+
+    // initialize tests
+    if (R_CHANNEL && !R_CHANNEL.some(n => n === interaction.commandName)) return [1, R_CHANNEL];
+    if (R_COMMAND && !R_COMMAND.some(id => id === interaction.channel.id)) return [2, R_COMMAND];
+
+}
+
+async function wrongChannelMessage(interaction, [code, list]) {
+    console.log({code, list});
+    // console.log(map);
+    const description = (
+        code == 1
+        ? '**This channel is only for the following commands:**\n'
+        + list.map(v => "`/" + v + "`").join('\n')
+        + "\n\n**Please only use these commands in this channel!**\nThank you! ❣️"
+        : '**This command can only be used in the following channels:**\n'
+        + list.map(v => "<#" + v + ">").join('\n')
+        + "\n\n**Please only use these commands in this channel!**\nThank you! ❣️"
+    )
+    return await interaction.reply({
+        ephemeral: true,
+        embeds: [new MessageEmbed()
+            .setColor('BLUE')
+            .setTitle('❗ __Woah There!__')
+            .setDescription(description)]
+    })
 }

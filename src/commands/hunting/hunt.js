bd2b397e9996840048ee315d77db07dc7afff6a6
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2022-06-04 18:48:02 -0600

    Implemented location-specific hunting rather than a catch-all

--- src/commands/hunting/hunt.js
+++ src/commands/hunting/hunt.js
@@ -1,34 +1,117 @@
 const HuntManager = require('../../util/Hunting/HuntManager')
-const { ApplicationCommandOptionType : dTypes } = require('discord-api-types/v10');
+const { ApplicationCommandOptionType : CommandTypes } = require('discord-api-types/v10');
 const { BaseCommandInteraction } = require('discord.js');
+const LOCATION_TEMPLATE = {
+
+}
 
 module.exports = {
     name: 'hunt',
     description: 'Hunt for food!',
     options: [
         {
-            name: 'current-territory',
-            description: 'The current territory you are hunting in.',
-            type: dTypes.String,
-            required: true,
-            choices: [
+            name: 'unforgiven',
+            description: 'Hunt within the Unforgiven\'s territory.',
+            type: CommandTypes.Subcommand,
+            options: [
                 {
-                    name: 'The Unforgiven',
-                    value: 'unforgiven'
-                },
+                    name: 'location',
+                    description: 'Where within this territory you will be hunting.',
+                    type: CommandTypes.String,
+                    required: true,
+                    choices: [
+                        {
+                            name: 'Outpost Rock',
+                            value: 'outpost-rock',
+                        },
+                        {
+                            name: 'By the gorge',
+                            value: 'gorge',
+                        },
+                        {
+                            name: 'Barn',
+                            value: 'barn',
+                        },
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'thunderclan',
+            description: 'Hunt within Thunderclan\'s territory.',
+            type: CommandTypes.Subcommand,
+            options: [
                 {
-                    name: 'River-Clan',
-                    value: 'riverclan'
-                },
+                    name: 'location',
+                    description: 'Where within this territory you will be hunting.',
+                    type: CommandTypes.String,
+                    required: true,
+                    choices: [
+                        {
+                            name: 'Snake Rocks',
+                            value: 'snake-rocks',
+                        },
+                        {
+                            name: 'Sandy Hollow',
+                            value: 'sandy-hollow',
+                        },
+                        {
+                            name: 'By the Thunderpath',
+                            value: 'thunderpath',
+                        },
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'shadowclan',
+            description: 'Hunt within Shadowclan\'s territory.',
+            type: CommandTypes.Subcommand,
+            options: [
                 {
-                    name: 'Shadow-Clan',
-                    value: 'shadowclan'
-                },
+                    name: 'location',
+                    description: 'Where within this territory you will be hunting.',
+                    type: CommandTypes.String,
+                    required: true,
+                    choices: [
+                        {
+                            name: 'Burnt Sycamore',
+                            value: 'burnt-sycamore',
+                        },
+                        {
+                            name: 'Pond',
+                            value: 'pond',
+                        },
+                        {
+                            name: 'Stream',
+                            value: 'river',
+                        },
+                        {
+                            name: 'Carrion Place',
+                            value: 'carrion-place',
+                        },
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'riverclan',
+            description: 'Hunt within Riverclan\'s territory.',
+            type: CommandTypes.Subcommand,
+            options: [
                 {
-                    name: 'Thunder-Clan',
-                    value: 'thunderclan'
-                },
-            ],
+                    name: 'location',
+                    description: 'Where within this territory you will be hunting.',
+                    type: CommandTypes.String,
+                    required: true,
+                    choices: [
+                        {
+                            name: 'River',
+                            value: 'river',
+                        }
+                    ]
+                }
+            ]
         },
     ],
     /**
@@ -40,7 +123,8 @@ module.exports = {
         await interaction.deferReply({ ephemeral: false });
 
         // track territory
-        const territory = interaction.options.getString('current-territory');
+        const territory = interaction.options.getSubcommand();
+        const location = interaction.options.getString('location');
         
         // pull user from the database
         const hunter = await HuntManager.FetchUser(interaction.user.id);
@@ -62,7 +146,7 @@ module.exports = {
         // roll for track
         const trackRoll = HuntManager.rollTrack(20);
         const catchRoll = HuntManager.rollCatch(20);
-        const prey = HuntManager.generatePrey(territory, 3);
+        const prey = HuntManager.generatePrey(location, 3);
 
         // display result
         return HuntManager.generateAndDisplayResults(
@@ -70,6 +154,7 @@ module.exports = {
             hunter,
             server,
             territory,
+            location,
             trackRoll,
             catchRoll,
             prey

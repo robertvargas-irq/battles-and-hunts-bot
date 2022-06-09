b263e14e550f2744a009a0e1ce4d94c188a8a73a
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2022-06-09 04:00:46 -0600

    Split user hunting results and excuse count to member for easier handling

---
+++ src/database/schemas/member.js
@@ -0,0 +1,107 @@
+const mongoose = require('mongoose');
+const { Schema } = require('mongoose');
+
+const MemberSchema = new Schema({
+    guildId: {
+        type: String,
+        required: true,
+    },
+    userId: {
+        type: String,
+        required: true,
+    },
+    excuses: {
+        approved: {
+            total: {
+                type: Number,
+                default: 0,
+            },
+            absence: {
+                type: Number,
+                default: 0,
+            },
+            late: {
+                type: Number,
+                default: 0,
+            },
+            left_early: {
+                type: Number,
+                default: 0,
+            },
+        },
+        denied: {
+            total: {
+                type: Number,
+                default: 0,
+            },
+            absence: {
+                type: Number,
+                default: 0,
+            },
+            late: {
+                type: Number,
+                default: 0,
+            },
+            left_early: {
+                type: Number,
+                default: 0,
+            },
+        },
+    },
+    hunting: {
+        contributions: {
+            preyCount: {
+                type: Number,
+                default: 0,
+            },
+            preyWeight: {
+                type: Number,
+                default: 0,
+            }
+        },
+        hunts: {
+            successful: {
+                type: Number,
+                default: 0,
+            },
+            unsuccessful: {
+                type: Number,
+                default: 0,
+            }
+        },
+        fullInventoryTrips: {
+            type: Number,
+            default: 0,
+        },
+        trips: {
+            type: Number,
+            default: 0,
+        }
+    }
+});
+
+/**
+ * @typedef {{
+ * guildId: string,
+ * userId: string,
+ * excuses: {
+ *      approved: number,
+ *      denied: number,
+ * },
+ * hunting: {
+ *      contributions: {
+ *          preyCount: number,
+ *          preyWeight: number,
+ *      },
+ *      hunts: {
+ *          successful: number,
+ *          unsuccessful: number,
+ *      },
+ *      fullInventoryTrips: number
+ *      trips: Number,
+ * },
+ * }} Member
+ * @type {Member}
+ * 
+*/
+module.exports = mongoose.model('Member', MemberSchema);

a6ffe8cf70515f6a1c0f1dc01f47b76ae58bd05c
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2022-06-10 19:57:27 -0600

    Properly logged connections and caching events and errors

--- src/main.js
+++ src/main.js
@@ -12,56 +12,70 @@ const ServerCache = require('./util/Server/ServerCache');
 const ExcuseCache = require('./util/Excused/ExcuseCache');
 dotenv.config({ path: '../.env' });
 
-// initialize Discord client then connect to the mongodb database
-let client = new BotClient({
+// initialize Discord client
+const client = new BotClient({
     intents: [ Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES ],
     partials: ["CHANNEL"]
 });
+
+// connect to the MongoDB database
+console.log('🧩 Connecting to the database...')
 require('./database/connect.js')().then(() => {
-    // cache languages
-    Language.LoadLanguages().then((cache) => console.log(
-        'Languages successfully cached',
-        'Map(' + cache.size + ')'
-    ));
 
-    // cache characters
-    CharacterCache.CacheCharacters().then((cache) => {
-        console.log(
-            'Characters successfully cached',
-            'Guilds(' + cache.size + ') {\n',
-            Array.from(cache).map(([guildId, data]) => `\t'${guildId}' => Map(${data.size})`).join('\n'),
-            '\n}'
-        );
-    });
+    // inform success then initialize the cache
+    console.log('✅ Database successfully connected!\n|| Caching in progress...');
+    Promise.all([
 
-    // cache members
-    MemberCache.CacheMembers().then((cache) => {
-        console.log(
-            'Members successfully cached',
-            'Guilds(' + cache.size + ') {\n',
-            Array.from(cache).map(([guildId, data]) => `\t'${guildId}' => Map(${data.size})`).join('\n'),
-            '\n}'
-        );
-    });
-
-    // cache servers
-    ServerCache.CacheServers().then((cache) => {
-        console.log(
-            'Servers successfully cached',
+        // cache languages
+        Language.LoadLanguages().then((cache) => console.log(
+            '\tLanguages successfully cached',
             'Map(' + cache.size + ')'
-        );
-    });
+        )),
+    
+        // cache characters
+        CharacterCache.CacheCharacters().then((cache) => {
+            console.log(
+                '\tCharacters successfully cached',
+                'Guilds(' + cache.size + ') {\n',
+                Array.from(cache).map(([guildId, data]) => `\t'${guildId}' => Map(${data.size})`).join('\n'),
+                '\n\t}'
+            );
+        }),
+    
+        // cache members
+        MemberCache.CacheMembers().then((cache) => {
+            console.log(
+                '\tMembers successfully cached',
+                'Guilds(' + cache.size + ') {\n',
+                Array.from(cache).map(([guildId, data]) => `\t\t'${guildId}' => Map(${data.size})`).join('\n'),
+                '\n\t}'
+            );
+        }),
+    
+        // cache servers
+        ServerCache.CacheServers().then((cache) => {
+            console.log(
+                '\tServers successfully cached',
+                'Map(' + cache.size + ')'
+            );
+        }),
+    
+        // cache excuses
+        ExcuseCache.CacheExcuses().then((cache) => {
+            console.log(
+                '\tExcuses successfully cached',
+                'Map(' + cache.size + ')'
+            );
+        }),
 
-    // cache excuses
-    ExcuseCache.CacheExcuses().then((cache) => {
-        console.log(
-            'Excuses successfully cached',
-            'Map(' + cache.size + ')'
-        );
-    })
+    ]).then(() => console.log('✅ All data successfully cached.')).catch(e => console.error('There was an error during the caching process.\n' + e));
+
 
 });
 
 
-// login
-client.login( process.env.DISCORD_TOKEN ).then(() => console.log('Client successfully logged in.'));
+// client login
+console.log('🧩 Logging into the client...');
+client.login( process.env.DISCORD_TOKEN )
+    .then(() => console.log('✅ Client successfully logged in.'))
+    .catch(e => console.error('There was an error logging into the Discord client.\n' + e));

8077b2a57ca5c72ae14714b314c0b98fb9e92eb4
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2022-07-14 04:33:58 -0600

    Added Submit handler

--- src/events/interactions/routes/button/charactermenu.js
+++ src/events/interactions/routes/button/charactermenu.js
@@ -1,5 +1,7 @@
 const { ButtonInteraction, MessageEmbed } = require('discord.js');
 const CharacterMenu = require('../../../../util/CharacterMenu/CharacterMenu');
+const SubmissionHandler = require('../../../../util/Submissions/SubmissionHandler');
+const CoreUtil = require('../../../../util/CoreUtil');
 const Player = require('../../../../util/Account/Player');
 
 /** @param {ButtonInteraction} button */
@@ -15,14 +17,32 @@ module.exports = async (button) => {
         + '\n> This may have been caused by another editor being opened, or the bot having restarted.'
     });
 
-    // ensure editing is valid
-    if (!active.isAdmin && !Player.allowedToEdit(active.interaction.guild.id, active.interaction.user.id)
-    && editTarget != 'INFO')
-        return button.reply({
-            embeds: [new MessageEmbed({
-                title: '🔒 Your editing has been locked.'
-            })]
-        });
-    return active.displayEditModal(button, editTarget);
+    // route
+    switch (action) {
+        case 'SUBMIT': {
+            const server = CoreUtil.Servers.cache.get(button.guild.id);
+            if (active.character.approved) return button.reply({
+                ephemeral: true,
+                embeds: [new MessageEmbed({
+                    color: 'YELLOW',
+                    title: '⚠️ Woah wait-!',
+                    description: '> It looks like this character has already been approved!'
+                })]
+            })
+            return SubmissionHandler.handleSubmission(button, active.character, server);
+        }
+        case 'EDIT': {
+            // ensure editing is valid
+            if (!active.isAdmin && !Player.allowedToEdit(active.interaction.guild.id, active.interaction.user.id)
+            && editTarget != 'INFO')
+                return button.reply({
+                    embeds: [new MessageEmbed({
+                        title: '🔒 Your editing has been locked.'
+                    })]
+                });
+            return active.displayEditModal(button, editTarget);
+        }
+    }
+
 
 }

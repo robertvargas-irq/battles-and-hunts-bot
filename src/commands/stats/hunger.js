b7512a517088d11476cf3607afaca55d5b69a7e0
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2022-07-15 04:04:25 -0600

    Moved hard-coded visuals to json; Prettified embed

--- src/commands/stats/hunger.js
+++ src/commands/stats/hunger.js
@@ -1,65 +1,54 @@
-const { BaseCommandInteraction, MessageEmbed } = require('discord.js');
+const { BaseCommandInteraction, MessageEmbed, Util: DiscordUtil } = require('discord.js');
 const CoreUtil = require('../../util/CoreUtil');
+const { colors, titles, descriptions, flairs } = require('./hungerVisuals.json');
 
 module.exports = {
     name: 'hunger',
     description: 'Quickly view your hunger!',
     /**@param {BaseCommandInteraction} interaction */
-    async execute( interaction ) {
+    async execute(interaction) {
         
         // if user is registered
         const found = CoreUtil.Characters.cache.get(interaction.guild.id, interaction.user.id);
         if (!found || !found.approved) return CoreUtil.NotRegistered(interaction);
 
-        // show health bar
+        // show hunger bar
+        const hungerRatio = found.currentHunger / found.stats.cat_size;
         interaction.reply({
-            embeds: [
-                new MessageEmbed()
-                    .setColor(getColor(found.currentHunger, found.stats.cat_size))
-                    .setTitle(getTitle(found.currentHunger, found.stats.cat_size))
-                    .setFields([
-                        {
-                            name: 'CURRENT HUNGER ' + (found.currentHunger < found.stats.cat_size ? '🍖' : '🦴'),
-                            value: `> ↣ \`${found.stats.cat_size - found.currentHunger}\` / \`${found.stats.cat_size}\``
-                        },
-                        {
-                            name: 'Last Ate At',
-                            value: '> ' + (found.lastAteAt > 0 ? '<t:' + found.lastAteAt + '>, roughly <t:' + found.lastAteAt + ':R>' : 'Hmm... can\'t remember...'),
-                        },
-                    ])
-                    .setFooter({ text: '🍃 This hunger stat is canon.' }),
-            ]
+            embeds: [new MessageEmbed({
+                color: getColor(hungerRatio),
+                title: '⟪' + getFlair(hungerRatio) + '⟫ ' + getTitle(hungerRatio),
+                description: '*' + getDescription(hungerRatio) + '*',
+                fields: [
+                    {
+                        name: 'Current Hunger',
+                        value: `> ↣ \`${found.stats.cat_size - found.currentHunger}\` / \`${found.stats.cat_size}\` `
+                        + `**(**${Math.floor((1 - hungerRatio) * 100)}%**)**`,
+                        inline: true,
+                    },
+                    {
+                        name: '▣'.repeat(Math.floor((1 - hungerRatio) * 10)) + '▢'.repeat(Math.floor(hungerRatio * 10)),
+                        value: '\u200B',
+                        inline: true,
+                    },
+                    {
+                        name: 'Last Ate At',
+                        value: '> ' + (found.lastAteAt > 0 ? '<t:' + found.lastAteAt + '>, roughly <t:' + found.lastAteAt + ':R>' : 'Hmm... can\'t remember...'),
+                    },
+                ],
+                footer: { text: '🍃 This hunger stat is canon.' }
+            })]
         });
     
     },
 };
 
 
-// ! PLEASE FORGIVE ME MY BRAIN FRIED
-const colors = [
-    'AQUA',
-    'YELLOW',
-    'RED',
-    'NOT_QUITE_BLACK'
-];
 
-const titles = [
-    '🍖 Satiated',
-    '🍴 Could go for a bite',
-    '⏳ Not feeling too good...',
-    '🦴 I might starve soon...'
-]
-
-function getColor(hunger, cat_size) {
-    if (hunger == 0) return colors[0];
-    if (hunger == cat_size) return colors[3];
-    if (hunger == cat_size - 1) return colors[2];
-    return colors[1];
-}
-
-function getTitle(hunger, cat_size) {
-    if (hunger == 0) return titles[0];
-    if (hunger == cat_size) return titles[3];
-    if (hunger == cat_size - 1) return titles[2];
-    return titles[1];
-}
+/*
+ * Helper functions for visuals
+ */
+const getColor = (ratio) => CoreUtil.GetColorFromRatio(colors, ratio);
+const getDescription = (ratio) => CoreUtil.GetArrayElementFromRatio(descriptions, ratio);
+const getTitle = (ratio) => CoreUtil.GetArrayElementFromRatio(titles, ratio);
+const getFlair = (ratio) => CoreUtil.GetArrayElementFromRatio(flairs, ratio);

55809d4cc22c270448f02768a414d78b8c93fa85
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2022-07-15 07:25:52 -0600

    Extracted Hunger Embed to HungerVisuals.generateHungerEmbed()

--- src/commands/stats/hunger.js
+++ src/commands/stats/hunger.js
@@ -1,6 +1,6 @@
 const { CommandInteraction, MessageEmbed } = require('discord.js');
 const CoreUtil = require('../../util/CoreUtil');
-const { colors, titles, descriptions, flairs } = require('./hungerVisuals.json');
+const HungerVisuals = require('../../util/Hunting/HungerVisuals');
 
 module.exports = {
     name: 'hunger',
@@ -13,43 +13,9 @@ module.exports = {
         if (!found || !found.approved) return CoreUtil.NotRegistered(interaction);
 
         // show hunger bar
-        const hungerRatio = found.currentHunger / found.stats.cat_size;
         interaction.reply({
-            embeds: [new MessageEmbed({
-                color: getColor(hungerRatio),
-                title: '⟪' + getFlair(hungerRatio) + '⟫ ' + getTitle(hungerRatio),
-                description: '*' + getDescription(hungerRatio) + '*',
-                thumbnail: { url: found.icon ?? interaction.member.displayAvatarURL({ dynamic: true }) },
-                fields: [
-                    {
-                        name: 'Current Hunger',
-                        value: `> ↣ \`${found.stats.cat_size - found.currentHunger}\` / \`${found.stats.cat_size}\` `
-                        + `**(**${Math.floor((1 - hungerRatio) * 100)}%**)**`,
-                        inline: true,
-                    },
-                    {
-                        name: '▣'.repeat(Math.floor((1 - hungerRatio) * 10)) + '▢'.repeat(Math.floor(hungerRatio * 10)),
-                        value: '\u200B',
-                        inline: true,
-                    },
-                    {
-                        name: 'Last Ate At',
-                        value: '> ' + (found.lastAteAt > 0 ? '<t:' + found.lastAteAt + '>, roughly <t:' + found.lastAteAt + ':R>' : 'Hmm... can\'t remember...'),
-                    },
-                ],
-                footer: { text: '🍃 This hunger stat is canon' + ' : ' + (found.name ?? interaction.member.displayName + '\'s character')}
-            })]
+            embeds: [HungerVisuals.generateHungerEmbed(interaction.member, found)]
         });
     
     },
 };
-
-
-
-/*
- * Helper functions for visuals
- */
-const getColor = (ratio) => CoreUtil.GetColorFromRatio(colors, ratio);
-const getDescription = (ratio) => CoreUtil.GetArrayElementFromRatio(descriptions, ratio);
-const getTitle = (ratio) => CoreUtil.GetArrayElementFromRatio(titles, ratio);
-const getFlair = (ratio) => CoreUtil.GetArrayElementFromRatio(flairs, ratio);
--- src/util/Hunting/HungerVisuals.js
+++ src/util/Hunting/HungerVisuals.js
@@ -1,3 +1,4 @@
+const { MessageEmbed, GuildMember } = require('discord.js');
 const CoreUtil = require("../CoreUtil");
 const HungerVisualsData = require('./hungerVisuals.json');
 
@@ -6,6 +7,39 @@ class HungerVisuals {
     static getDescription = (ratio) => CoreUtil.GetArrayElementFromRatio(HungerVisualsData.descriptions, ratio);
     static getTitle = (ratio) => CoreUtil.GetArrayElementFromRatio(HungerVisualsData.titles, ratio);
     static getFlair = (ratio) => CoreUtil.GetArrayElementFromRatio(HungerVisualsData.flairs, ratio);
+
+    /**
+     * Generate a basic Hunger embed
+     * @param {GuildMember} interaction 
+     * @param {CharacterModel} character 
+     */
+    static generateHungerEmbed = (member, character) => {
+        const hungerRatio = character.currentHunger / character.stats.cat_size;
+        return new MessageEmbed({
+            color: HungerVisuals.getColor(hungerRatio),
+            title: '⟪' + HungerVisuals.getFlair(hungerRatio) + '⟫ ' + HungerVisuals.getTitle(hungerRatio),
+            description: '*' + HungerVisuals.getDescription(hungerRatio) + '*',
+            thumbnail: { url: character.icon ?? member.displayAvatarURL({ dynamic: true }) },
+            fields: [
+                {
+                    name: 'Current Hunger',
+                    value: `> ↣ \`${character.stats.cat_size - character.currentHunger}\` / \`${character.stats.cat_size}\` `
+                    + `**(**${Math.floor((1 - hungerRatio) * 100)}%**)**`,
+                    inline: true,
+                },
+                {
+                    name: '▣'.repeat(Math.floor((1 - hungerRatio) * 10)) + '▢'.repeat(Math.floor(hungerRatio * 10)),
+                    value: '\u200B',
+                    inline: true,
+                },
+                {
+                    name: 'Last Ate At',
+                    value: '> ' + (character.lastAteAt > 0 ? '<t:' + character.lastAteAt + '>, roughly <t:' + character.lastAteAt + ':R>' : 'Hmm... can\'t remember...'),
+                },
+            ],
+            footer: { text: '🍃 This hunger stat is canon' + ' : ' + (character.name ?? member.displayName + '\'s character')}
+        });
+    }
 }
 
 module.exports = HungerVisuals;

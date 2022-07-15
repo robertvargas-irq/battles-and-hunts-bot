2e7c7c9366973942a8ec3b1a024737cb696cf482
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2022-07-15 04:05:11 -0600

    Moved hard-coded values to json; prettified embed

--- src/commands/stats/health.js
+++ src/commands/stats/health.js
@@ -1,6 +1,8 @@
-const { BaseCommandInteraction, MessageEmbed } = require('discord.js');
+const { BaseCommandInteraction, MessageEmbed, Util: DiscordUtil } = require('discord.js');
 const { calculateMaxHealth } = require('../../util/Account/Player');
 const CoreUtil = require('../../util/CoreUtil');
+const ColorUtil = require('color2k');
+const { colors, titles, descriptions, flairs } = require('./healthVisuals.json');
 
 module.exports = {
     name: 'health',
@@ -13,52 +15,39 @@ module.exports = {
         if (!found || !found.approved) return CoreUtil.NotRegistered(interaction);
 
         // show health bar
+        const maxHealth = calculateMaxHealth(found.stats.constitution);
+        const healthRatio = found.currentHealth / maxHealth;
+        const description = getDescription(healthRatio);
         interaction.reply({
-            embeds: [
-                new MessageEmbed()
-                    .setColor(getColor(found.currentHealth, found.stats.constitution))
-                    .setTitle(getTitle(found.currentHealth, found.stats.constitution))
-                    .addField('CURRENT HEALTH ' + (found.currentHealth > 0 ? '💘' : '💔'), `> ↣ \`${found.currentHealth}\` / \`${calculateMaxHealth(found.stats.constitution)}\``),
-            ]
+            embeds: [new MessageEmbed({
+                color: getColor(healthRatio),
+                title: '⟪' + getFlair(healthRatio) + '⟫ ' + getTitle(healthRatio),
+                description: description ? '*' + description + '*' : '',
+                fields: [
+                    {
+                        name: 'Current Health',
+                        value: `> ↣ \`${found.currentHealth}\` / \`${calculateMaxHealth(found.stats.constitution)}\` `
+                        + `**(**${Math.floor(healthRatio * 100)}%**)**`,
+                        inline: true,
+                    },
+                    {
+                        name: '▣'.repeat(Math.floor(healthRatio * 10)) + '▢'.repeat(Math.floor((1 - healthRatio) * 10)),
+                        value: '\u200B',
+                        inline: true,
+                    },
+                ]
+            })]
         });
     
     },
 };
 
 
-// ! PLEASE FORGIVE ME MY BRAIN FRIED
-const colors = [
-    'AQUA',
-    'YELLOW',
-    'RED',
-    'DARK_RED',
-    'NOT_QUITE_BLACK'
-];
 
-const titles = [
-    '🌿 Serene',
-    '💫 A bit out of it',
-    '❤️‍🩹 Hrrk...',
-    '🩸 Help..',
-    '🪦'
-]
-
-function getColor(health, constitution) {
-    let maxHealth = calculateMaxHealth(constitution);
-
-    if (health >= maxHealth / 4 * 3) return colors[0]
-    else if (health > maxHealth / 2) return colors[1];
-    else if (health > maxHealth / 4) return colors[2];
-    else if (health > 0) return colors[3];
-    else return colors[4];
-}
-
-function getTitle(health, constitution) {
-    let maxHealth = calculateMaxHealth(constitution);
-
-    if (health >= maxHealth / 4 * 3) return titles[0]
-    else if (health > maxHealth / 2) return titles[1];
-    else if (health > maxHealth / 4) return titles[2];
-    else if (health > 0) return titles[3];
-    else return titles[4];
-}
+/*
+ * Helper functions for visuals
+ */
+const getColor = (ratio) => CoreUtil.GetColorFromRatio(colors, ratio);
+const getDescription = (ratio) => CoreUtil.GetArrayElementFromRatio(descriptions, ratio);
+const getTitle = (ratio) => CoreUtil.GetArrayElementFromRatio(titles, ratio);
+const getFlair = (ratio) => CoreUtil.GetArrayElementFromRatio(flairs, ratio);

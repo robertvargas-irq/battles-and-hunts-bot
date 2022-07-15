47435c90e5b66af476aac7719633b8f6bb64873e
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2022-07-15 07:03:51 -0600

    Extracted embed into HealthVisuals for reuse in other methods

--- src/commands/stats/health.js
+++ src/commands/stats/health.js
@@ -1,5 +1,4 @@
 const { BaseCommandInteraction, MessageEmbed, Util: DiscordUtil } = require('discord.js');
-const StatCalculator = require('../../util/Stats/StatCalculator');
 const CoreUtil = require('../../util/CoreUtil');
 const HealthVisuals = require('../../util/Battle/HealthVisuals');
 
@@ -14,30 +13,8 @@ module.exports = {
         if (!found || !found.approved) return CoreUtil.NotRegistered(interaction);
 
         // show health bar
-        const maxHealth = StatCalculator.calculateMaxHealth(found);
-        const healthRatio = found.currentHealth / maxHealth;
-        const description = HealthVisuals.getDescription(healthRatio);
         interaction.reply({
-            embeds: [new MessageEmbed({
-                color: HealthVisuals.getColor(healthRatio),
-                title: '⟪' + HealthVisuals.getFlair(healthRatio) + '⟫ ' + HealthVisuals.getTitle(healthRatio),
-                description: description ? '*' + description + '*' : '',
-                thumbnail: { url: found.icon ?? interaction.member.displayAvatarURL({ dynamic: true }) },
-                fields: [
-                    {
-                        name: 'Current Health',
-                        value: `> ↣ \`${found.currentHealth}\` / \`${StatCalculator.calculateMaxHealth(found)}\` `
-                        + `**(**${Math.floor(healthRatio * 100)}%**)**`,
-                        inline: true,
-                    },
-                    {
-                        name: '▣'.repeat(Math.floor(healthRatio * 10)) + '▢'.repeat(Math.floor((1 - healthRatio) * 10)),
-                        value: '\u200B',
-                        inline: true,
-                    },
-                ],
-                footer: { text: (found.name ?? interaction.member.displayName + '\'s character') },
-            })]
+            embeds: [HealthVisuals.generateHealthEmbed(interaction.member, found)]
         });
     
     },
